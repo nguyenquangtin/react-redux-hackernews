9ef3de50ba1452608dadb98764349f53a0d6930f
Tony Tin Nguyen <nguyenquangtin@gmail.com>
2018-10-10 09:12:06 +0700

    Add the first reducer and redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import store from './store';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 const stories = [
   {
@@ -23,11 +23,6 @@ const stories = [
 ];
 
 ReactDOM.render(
-  <App stories = {stories} onArchive={() => {}} />,
+  <App stories = {store.getState()} onArchive={() => {}} />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
