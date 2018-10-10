eab0e39a43c13cf20aab12d59e7295679d7ec5c4
Tony Tin Nguyen <nguyenquangtin@gmail.com>
2018-10-10 09:19:54 +0700

    Add Archive action

--- src/index.js
+++ src/index.js
@@ -2,30 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store';
 import App from './components/App';
+import { STORY_ARCHIVE } from './constants/actionTypes';
 import './index.css';
-
-const stories = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  }, {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
 
 ReactDOM.render(
   <App
     stories = {store.getState().storyState}
-    onArchive={() => {}}
+    onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
   />,
   document.getElementById('root')
 );
