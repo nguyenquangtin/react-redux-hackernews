ffc71b625d2028dc48187ad8ba008a8ee411de25
Tony Tin Nguyen <nguyenquangtin@gmail.com>
2018-10-10 09:22:40 +0700

    Add story selector

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store';
 import App from './components/App';
+import { getReadableStories } from './selectors/story';
 import { STORY_ARCHIVE } from './constants/actionTypes';
 import './index.css';
 
 ReactDOM.render(
   <App
-    stories = {store.getState().storyState}
+    stories = {getReadableStories(store.getState())}
     onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
   />,
   document.getElementById('root')
---
+++ src/selectors/story.js
@@ -0,0 +1,11 @@
+const isNotArchived = archivedIds => story =>
+  archivedIds.indexOf(story.objectID) === -1;
+
+const getReadableStories = ({
+  storyState,
+  archiveState
+}) => storyState.filter(isNotArchived(archiveState));
+
+export {
+  getReadableStories,
+};
